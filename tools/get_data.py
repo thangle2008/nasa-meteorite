a66c25c43be477c4a29fd582b971131c4d50fab7
Thang Le <thangminh.le208@gmail.com>
2016-10-16 01:07:44 -0400

    display data by normalized masses

--- tools/get_data.py
+++ tools/get_data.py
@@ -3,42 +3,51 @@ import json
 
 data = None
 
-url = "https://data.nasa.gov/resource/y77d-th95.json?$query=SELECT * WHERE year IS NOT NULL ORDER BY year DESC"
+url = "https://data.nasa.gov/resource/y77d-th95.json"
 
 response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
 
-res = [["series A", []],]
+res = [["0", []], ["10", []], ["100", []]]
 
 locations = dict()
-mag_min = float('inf')
-mag_max = 0.0
 
+# maxs and mins in 3 series
+mass_mins = [float('inf')] * 3
+mass_maxs = [0.0] * 3
+
+# process data
 for d in data:
+	# skip unknown location
 	if 'geolocation' not in d:
 		continue
-	lat = float(d['reclat'])
-	lon = float(d['reclong'])
 
-	# skip unknown locations
-	if lat == 0 and lon == 0:
-		continue
+	lon, lat = d['geolocation']['coordinates']
 
-	geol = (lon, lat)
-	if geol not in locations:
-		locations[geol] = 1.0
+	# if mass is unknown, assign 0
+	mass = 0.0
+	if 'mass' in d:
+		mass = float(d['mass'])
+
+	if mass < 10:
+		res[0][1] = res[0][1] + [lat, lon, mass]
+		mass_mins[0] = min(mass_mins[0], mass)
+		mass_maxs[0] = max(mass_maxs[0], mass)
+	elif 10 <= mass <= 100:
+		res[1][1] = res[1][1] + [lat, lon, mass]
+		mass_mins[1] = min(mass_mins[1], mass)
+		mass_maxs[1] = max(mass_maxs[1], mass)
 	else:
-		locations[geol] += 1.0
-	mag_min = min(mag_min, locations[geol])
-	mag_max = max(mag_max, locations[geol])
+		res[2][1] = res[2][1] + [lat, lon, mass]
+		mass_mins[2] = min(mass_mins[2], mass)
+		mass_maxs[2] = max(mass_maxs[2], mass)
 
-for coord in locations:
-	lon, lat = coord
-	# normalize the data and scale
-	mag = (locations[coord] - mag_min) / (mag_max - mag_min) + 0.05
-
-	res[0][1] = res[0][1] + [lat, lon, mag]
+# normalize the masses in each series
+for s in range(0, 3):
+	print len(res[s][1])
+	for i in range(2, len(res[s][1]), 3):
+		res[s][1][i] = (res[s][1][i] - mass_mins[s]) / (mass_maxs[s] - mass_mins[s])
 
 with open('data.json', 'w') as outfile:
 	json.dump(res, outfile)
